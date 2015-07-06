4d44fdc41ccb12b69a45f46268de10cd6a68b6b8
alan <alan@fancysandwiches.com>
2015-07-06 00:49:08 -0700

    Add persona_state, game, and last_messaged to Person class.

--- steam_alerts/person.py
+++ steam_alerts/person.py
@@ -6,3 +6,6 @@ class Person:
         self.name = name
         self.phone_number = phone_number
         self.steam_id = steam_id
+        self.persona_state = None
+        self.game = None
+        self.last_messaged = None
